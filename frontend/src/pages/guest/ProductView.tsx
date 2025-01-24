0d55101779f39814f7c1f9e58b0e6a82f0abfd0d
deilon <deilon@users.noreply.github.com>
2025-01-24 14:15:22 +0800

    Frontend: Add product overview details on ProductView.tsx

--- frontend/src/pages/guest/ProductView.tsx
+++ frontend/src/pages/guest/ProductView.tsx
@@ -1,8 +1,12 @@
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
-import Nav from "../Nav";
-import { Card, CardContent } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { Card, CardContent } from "@/components/ui/card";
+import Nav from "../Nav";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { Minus, Plus } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export default function ProductView() {
   return (
@@ -72,9 +76,40 @@ export default function ProductView() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-auto bg-gray-100 p-5">
-                    Product general description goes here
+
+                <div className="flex flex-col w-7/12 p-5">
+                    <h1 className="scroll-m-20 text-4xl font-bold lg:text-5xl">
+                        Product Name / Title
+                    </h1>
+                    <div className="flex items-center space-x-4 mt-4">
+                        <div className="rating text-yellow-500">
+                            <span className="text-lg text-gray-500 me-2">4.9</span>
+                            <i className='bx bxs-star'></i>
+                            <i className='bx bxs-star'></i>
+                            <i className='bx bxs-star'></i>
+                            <i className='bx bxs-star'></i>
+                            <i className='bx bxs-star-half'></i>
+                        </div>
+                        <Separator orientation="vertical" />
+                        <p className="text-lg text-gray-500">2.4k Ratings</p>
+                        <Separator orientation="vertical" />
+                        <p className="text-lg text-gray-500">10k+ Sold</p>
+                    </div>
+                    <p className="text-[35px] my-4 font-semibold">₱17,269</p>
+                    <div className="flex items-center">
+                        <span className="me-3 text-gray-500">Quantity:</span>
+                        <Button variant="outline" size="icon">
+                            <Minus />
+                        </Button>
+                        <Input className="w-[50px] mx-3 text-center" max="50" step="1" value="0" autoComplete="off" type="text" height="100%" />
+                        <Button variant="outline" size="icon">
+                            <Plus />
+                        </Button>
+                    </div>
+                    <span className="mt-4 text-sm text-gray-500">290 pieces available</span>
+                    <Button className="mt-4 w-[200px]">ADD TO CART</Button>
                 </div>
+
             </div>
 
         </div>
